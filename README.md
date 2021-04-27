9e7f551710abe1cb2beb9ca7a63a7485e53e5d82
Sean Workman <sean.workman.96@gmail.com>
2021-04-27 18:12:16 -0400

    Final

--- README.md
+++ README.md
@@ -20,3 +20,20 @@ Puzzle is not solved if:
 - Any row contains more than one of the same number from 1 to 9
 - Any column contains more than one of the same number from 1 to 9
 - Any 3x3 grid contains more than one of the same number from 1 to 9
+
+## Analysis
+	NBA (Easy)	SBA (Easy)	NBA (Med)	SBA (Med)	NBA (Hard)	SBA (Hard)	NBA (Evil)	SBA (Evil)
+1	0.063	    0.409	    0.556	    0.552	    0.812	    0.585	    1.592	    0.317
+2	0.092	    0.243	    0.947	    0.646	    0.389	    3.04	    1.665	    0.162
+3	0.351	    0.312	    0.3	        0.604	    4.312	    3.361	    0.297	    0.267
+4	0.502	    0.29	    0.897	    0.486	    3.264	    3.139	    1.935	    1.763
+5	0.243	    0.238	    0.591	    0.454	    1.054	    1.221	    1.74	    1.879
+6	0.119	    0.178	    0.625	    0.213	    0.315	    0.105	    0.227	    0.18
+7	0.06	    0.147	    0.731	    0.711	    0.508	    0.836	    1.801	    0.222
+8	0.256	    0.401	    0.346	    0.862	    3.987	    0.258	    1.852	    0.278
+9	0.257	    0.213	    0.824	    0.63	    1.875	    3.353	    0.228	    0.2355
+10	0.106	    0.391	    0.392	    0.479	    0.329	    0.187	    1.688	    1.606
+AVG	0.2049	    0.2822	    0.6209	    0.5637	    1.6845	    1.6085	    1.3025	    0.69095
+
+The Smart Backtracking Algorithm (forward-checking) algorithm outperformed the Naive Backtracking Algorithm on each puzzle except the first (easy) one.
+
--- sudoku.py
+++ sudoku.py
@@ -4,131 +4,7 @@ import time
 import sys
 import random
 
-rows = 5
-columns = 5
-goal = [
-    [1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10],
-    [11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20],
-    [21, 22, 23, 24, 0]
-]
-
-# rows = 3
-# columns = 3
-# goal = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 0]
-# ]
-
-initial = []
-
 quiet = False
-
-# Helper function to determine if the current state matches the goal state
-def goalTest(state):
-    failCount = 0
-    for i in range(rows):
-        for j in range(columns):
-            if state[i][j] != goal[i][j]:
-                failCount += 1
-    
-    if failCount > 0:
-        return False
-    else:
-        return True
-
-# Helper function to get coordinates of empty space
-def getEmptySpace(state):
-    emptySpace = ()
-    for i in range(rows):
-        for j in range(columns):
-            if state[i][j] == 0:
-                emptySpace = (i, j)
-                break
-    return emptySpace
-
-# Helper function to gather coordinates of neighbors of empty space
-def getNeighbors(state):
-    neighbors = []
-    emptySpace = getEmptySpace(state)
-
-    # Neighbor above
-    if emptySpace[0] - 1 >= 0:
-        neighbors.append((emptySpace[0] - 1, emptySpace[1]))
-
-    # Neighbor below
-    if emptySpace[0] + 1 < rows:
-        neighbors.append((emptySpace[0] + 1, emptySpace[1]))
-
-    # Neighbor left
-    if emptySpace[1] - 1 >= 0:
-        neighbors.append((emptySpace[0], emptySpace[1] - 1))
-
-    # Neighbor right
-    if emptySpace[1] + 1 < columns:
-        neighbors.append((emptySpace[0], emptySpace[1] + 1))
-    
-    # print(neighbors)
-    return neighbors
-
-# Helper function to build options based on neighbors
-def buildOptions(state):
-    neighbors = getNeighbors(state)
-    emptySpace = getEmptySpace(state)
-    # print(emptySpace)
-    options = []
-    for i in neighbors:
-        tempState = []
-        tempState = deepcopy(state)
-        tempElement = tempState[i[0]][i[1]]
-        # print(i)
-        tempState[emptySpace[0]][emptySpace[1]] = tempElement
-        tempState[i[0]][i[1]] = 0
-        options.append(tempState)
-        # print(state)
-    return options
-
-# Helper funciton to determine if two states are the same
-def areEqualStates(a, b):
-    failCount = 0
-    for i in range(rows):
-        for j in range(columns):
-            if a[i][j] != b[i][j]:
-                failCount += 1
-                break
-
-    if failCount > 0:
-        return False
-    else:
-        return True
-
-# Helper function to determine if subject is in the queue
-def isInQueue(q, subject):
-    tempQ = Queue()
-    tempQ = q
-    while q.size() > 0:
-        a = tempQ.removefromq()
-        if areEqualStates(a, subject):
-            return True
-    return False
-
-# Helper function to check is a neighbor is in a list 
-def neighborInExplored(neighbor, explored):
-    failCount = 0
-    matches = False
-    for exploredList in explored:
-        failCount = 0
-        for i in range(rows):
-            for j in range(columns):
-                if exploredList[i][j] != neighbor[i][j]:
-                    failCount += 1
-        
-        if failCount == 0:
-            matches = True
-            break
-    return matches
 
 # Helper funciton to print the state in a readable order
 def printState(state):
@@ -136,92 +12,6 @@ def printState(state):
     for i in state:
         print(i)
     print('------------')
-
-# Helper function to generate initial state
-def generateInitial():
-    values = random.sample(range(0, (rows * columns)), (rows * columns))
-    valuesIndex = 0
-    result = []
-    for i in range(rows):
-        list1 = []
-        for j in range(columns):
-            list1.append(values[valuesIndex])
-            valuesIndex += 1
-        result.append(list1)
-    return result
-
-
-# Breadth First Search
-# Queue: FIFO
-def bfs(initial):
-    # Create frontier queue
-    frontier = Queue()
-    explored = []
-    frontierList = []
-
-    # Add initial state to frontier
-    frontier.addtoq(initial)
-    frontierList.append(initial)
-
-    # Go through the queue until it is empty
-    while frontier.size() > 0:
-        # Pop the element from the queue
-        state = frontier.removefromq()
-
-        # Add it to explored
-        explored.append(state)
-
-        if not quiet:
-            printState(state)
-
-        # Check if the current state is the goal state
-        if goalTest(state):
-            return "Success"
-
-        # Get options from current state and neighbors
-        options = buildOptions(state)
-
-        for neighbor in options:
-            tempFrontier = Queue()
-            tempFrontier = deepcopy(frontier)
-            if not neighborInExplored(neighbor, explored) and not neighborInExplored(neighbor, frontierList):
-                frontier.addtoq(neighbor)
-                frontierList.append(neighbor)       
-    return "FAILURE"
-
-# Depth First Search
-# Stack: LIFO
-def dfs(initial):
-    # Create frontier stack
-    frontier = Stack()
-    explored = []
-    frontierList = []
-    # Add initial state to frontier
-    frontier.add(initial)
-    frontierList.append(initial)
-    # Go through the stack until it is empty 
-    while frontier.size() > 0:
-        # Pop the element from the stack
-        state = frontier.remove()
-
-        # Add it to explored
-        explored.append(state)
-
-        if not quiet:
-            printState(state)
-
-        # Check if the current state is the goal state
-        if goalTest(state):
-            return "Success"
-
-        # Get options from current state and neighbors
-        options = buildOptions(state)
-        # print(options)
-        for neighbor in options:
-            if not neighborInExplored(neighbor, explored) and not neighborInExplored(neighbor, frontierList):
-                frontier.add(neighbor)
-                frontierList.append(neighbor)
-    return "FAILURE"
 
 # Checks if the value will work in the puzzle
 # p: The puzzle
@@ -249,7 +39,7 @@ def doesValueWork(p, val, position):
         return False
 
     return True
-
+    
 # Helper function to determine if the puzzle is solved
 def isPuzzleSolved(puzzle):
     for i in puzzle:
@@ -264,100 +54,120 @@ def triedAll(tried):
         return True
     return False
 
+# Finds an empty location (0) and returns the location
+# If no empty locations return (-1, -1)
+def findEmpty(puzzle):
+    position = (-1, -1)
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            if puzzle[i][j] == 0:
+                position = (i, j)
+                return position
+    return position
+
+
+# Helper function to generate random list of values 1-9
+def generateRandomList():
+    values = random.sample(range(1, 10), 9)
+    return values
+
+# Generate a dictionary with 
+def generateFCDict(puzzle):
+    fcDict = {}
+    emptyLocs = []
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            if puzzle[i][j] == 0:
+                emptyLocs.append((i, j))
+    
+    randList = []
+    for i in emptyLocs:
+        randList = generateRandomList()
+        # fcDict[i] = randList
+        fcDict[i] = []
+        for j in randList:
+            if doesValueWork(puzzle, j, i):
+                fcDict[i].append(j)
+    return fcDict
+
+
 # Implement a naïve backtracking algorithm. The selection of variables and 
 # assignment of values can be done either in order or randomly.
 def nba(puzzle):
-    # print(doesValueWork(puzzle, 2, (2, 1)))
-    tried = []
-    positionsFilled = []
-    backtrack = False
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[i])):
-            works = False
-            # Handle backtracking
-            # print("BACKTRACK1", backtrack)
-            if backtrack:
-                if len(positionsFilled) == 0:
-                    return "FAILURE"
-                # Backtrack to the last filled position
-                backPos = positionsFilled.pop()
-                i = backPos[0]
-                j = backPos[1]
-                # print((i, j))
-                position = backPos
-                # print('POSITION1', position)
-                tried = []
-                tried.append(puzzle[i][j])
-                puzzle[i][j] = 0
-                backtrack = False
-                # print("BACKTRACKING STATE")
-                # printState(puzzle)
-                # print("BACKTRACKING STATE")
-            # print("BACKTRACK2", backtrack)
+    if not quiet:
+        printState(puzzle)
 
-            # If the element is a 0 it hasn't been filled yet 
-            if puzzle[i][j] == 0:
-                # Store the position to track which have been filled
-                position = (i, j)
-                # print('POSITION2', position)
-                
-
-                # pick a random number 1 to 9
-                val = random.randint(1, 9)
-                # pick a random number that hasn't been tried yet
-                while val in tried:
-                    # If every number has been tried, backtrack
-                    if triedAll(tried):
-                        backtrack = True
-                        break
-                    val = random.randint(1, 9)
-                if backtrack:
-                    continue
-
-                # Test to see if this random value works in that position
-                works = doesValueWork(puzzle, val, position)
-                tried.append(val)
-                # Keep trying values until one works
-                while not works:
-                    # pick a random number that hasn't been tried yet
-                    while val in tried:
-                        if triedAll(tried):
-                            backtrack = True
-                            break
-                        val = random.randint(1, 9)
-                    # No more values to try so backtrack
-                    if backtrack:
-                        break
-                    # Test to see if this random value works in that position
-                    works = doesValueWork(puzzle, val, position)
-                    tried.append(val)
-                    # print(positionsFilled)
-                    # print(tried)
-                    # print("BACKTRACK", backtrack)
-                # Backtrack
-                if backtrack:
-                    continue
-
-                puzzle[i][j] = val
-                positionsFilled.append(position)
-                printState(puzzle)
-                # if backtrack:
-                #     break
-                
-            tried = []
+    # Set an arbitrary location
+    position = (-1, -1)
     
-    return isPuzzleSolved(puzzle)
+    # if no empty locations, the puzzle is solved
+    position = findEmpty(puzzle)
+    if position[0] == -1:
+        return True
+    
+    # Generate list of random values 1-9 to be tried 
+    randomVals = generateRandomList()
+    for val in randomVals:
+        # Check if the value works in the specified position
+        if doesValueWork(puzzle, val, position):
+            # Set the position to the val
+            puzzle[position[0]][position[1]] = val
+
+            # Success
+            if nba(puzzle):
+                printState(puzzle)
+                return True
+
+            # Failure, revert puzzle
+            puzzle[position[0]][position[1]] = 0
+
+    # Backtrack
+    return False
 
 # Incorporate at least one strategy of minimum remaining values (MRV), 
 # least constraining value (LCV), and forward checking in your backtracking algorithm.
 def sba(puzzle):
+    if not quiet:
+        printState(puzzle)
 
-    return "FAILURE"
+    # Generate dictionary for forward-checking
+    fcDict = generateFCDict(puzzle)
+    # Set an arbitrary location
+    position = (-1, -1)
+    
+    # if no empty locations, the puzzle is solved
+    position = findEmpty(puzzle)
+
+    if position[0] == -1:
+        return True
+
+    # Instead of random values use the values from the forward checking dictionary
+    remainingVals = fcDict[position]
+
+    for val in remainingVals:
+        # Check if the value works in the specified position
+        if doesValueWork(puzzle, val, position):
+            # Set the position to the val
+            puzzle[position[0]][position[1]] = val
+
+            # Success
+            if nba(puzzle):
+                printState(puzzle)
+                return True
+
+            # Failure, revert puzzle
+            puzzle[position[0]][position[1]] = 0
+
+    # Backtrack
+    return False
 
 # Parses Sudoku.txt
 def parseInput():
     f = open("Sudoku.txt", "r")
-    # Read the first line to remove the 'Sudoku 01'
+    # f = open("SudokuMedium.txt", "r")
+    # f = open("SudokuHard.txt", "r")
+    # f = open("SudokuEvil.txt", "r")
+    # Read the first line to remove the title line ('Sudoku 01')
     f.readline()
     count = 0
     inputList = []
@@ -373,15 +183,23 @@ def parseInput():
 
 def solve(mode):
     puzzle = parseInput()
-    printState(puzzle)
+    # printState(puzzle)
     if mode.lower() == "nba":
         start_time = time.time()
-        print(nba(puzzle))
+        if nba(puzzle):
+            printState(puzzle)
+            print("SUCCESS")
+        else:
+            print("FAILURE, NO SOLUTION")
         elapsed_time = time.time() - start_time
         print('Naive Backtracking Algorithm finished in {}'.format(elapsed_time))
     else:
         start_time = time.time()
-        print(sba(puzzle))
+        if sba(puzzle):
+            printState(puzzle)
+            print("SUCCESS")
+        else:
+            print("FAILURE, NO SOLUTION")
         elapsed_time = time.time() - start_time
         print('Smart Backtracking Algorithm finished in {}'.format(elapsed_time))
 
